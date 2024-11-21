82aecc4302b64259eeaa456d2983b2191f772a90
Mohammed <mammegentleman@gmail.com>
2024-11-21 22:38:02 +0300

    Add files via upload

---
+++ Mohammed.js
@@ -0,0 +1,8 @@
+document.querySelector('.button.connect').addEventListener('click', function() {
+    window.open("https://www.linkedin.com/in/mohammed-abdi-641917202/", "_blank");
+});
+
+document.querySelector('.button.message').addEventListener('click', function() {
+    window.open("https://www.linkedin.com/in/mohammed-abdi-641917202/", "_blank");
+});
+
